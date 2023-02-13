aa3e5cebab5c9b839c68d8370c373e0ed50fe0dc
zhanglei36 <zhanglei36@asiainfo.com>
2023-02-13 18:34:26 +0800

    init

--- src/main/java/io/github/coolbeevip/license/maven/plugin/cli/JoinMgr.java
+++ src/main/java/io/github/coolbeevip/license/maven/plugin/cli/JoinMgr.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright © 2020 Lei Zhang (zhanglei@apache.org)
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.coolbeevip.license.maven.plugin.cli;
 
 import com.opencsv.CSVParserBuilder;
@@ -76,7 +92,7 @@ public class JoinMgr {
 
     FileWriter fileWriter = new FileWriter(outputPath.toFile());
     try (PrintWriter printWriter = new PrintWriter(fileWriter)) {
-      printWriter.println("artifact\t"+csvArtifactMap.keySet().stream().collect(Collectors.joining("\t")));
+      printWriter.println("artifact\t" + csvArtifactMap.keySet().stream().collect(Collectors.joining("\t")));
       for (String[] line : versions) {
         printWriter.print(Arrays.stream(line).collect(Collectors.joining("\t")) + "\n");
       }
