86f4a67ed86d19bcfad04fa62350ad177f96ced9
zhanglei36 <zhanglei36@asiainfo.com>
2023-02-13 19:11:48 +0800

    init

--- src/main/java/io/github/coolbeevip/license/maven/plugin/cli/Main.java
+++ src/main/java/io/github/coolbeevip/license/maven/plugin/cli/Main.java
@@ -33,7 +33,7 @@ public class Main {
     JoinMgr mgr = new JoinMgr(cliArgs.separator);
     File[] csvFiles = Paths.get(cliArgs.path).toFile().listFiles();
     for (File file : csvFiles) {
-      if (file.getName().endsWith(".csv")) {
+      if (file.getName().toLowerCase().endsWith(".csv")) {
         mgr.addCsv(file.toPath());
       }
     }
